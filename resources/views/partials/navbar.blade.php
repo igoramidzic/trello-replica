39b19dd33333257c77636311ed4e6dbe8584a021
Igor Amidzic <amidzicigor@yahoo.com>
2017-05-04 16:32:15 -0400

    Add navbar links

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -3,24 +3,27 @@
   <!-- Content container -->
   <div class="row container-fluid">
     <!-- Boards and Search Form -->
-    <div class="holder left-container col-lg-5 col-md-5 col-sm-5 col-xs-5">
+    <div class="left-container col-lg-5 col-md-5 col-sm-5 col-xs-5">
       <div class="content-holder">
-        <a href="/" class="logo"><i class="fa fa-th-large"></i> Boards</a>
+        <a href="#" class="boards boards-menu-link"><i class="fa fa-th-large"></i> Boards</a>
       </div>
     </div>
 
     <!-- Logo -->
     <div class="holder center-container col-lg-2 col-md-2 col-sm-2 col-xs-2">
-
       <div class="content-holder">
-        <a href="/" class="logo ">Trello <i class="fa fa-angle-up"></i></a>
+        <a href="/" class="logo"><img src="img/trello-logo.png" alt=""></a>
       </div>
     </div>
 
     <!-- Menu, Notifications, and Profile -->
     <div class="holder right-container col-lg-5 col-md-5 col-sm-5 col-xs-5">
       <div class="content-holder">
-        <a href="/" class="logo"><i class="fa fa-th-large"></i> Boards</a>
+        <div class="">
+
+        </div>
+        <a href="#" class="notifs-btn boards-menu-link"><i class="fa fa-bell-o"></i></a>
+        <a href="#" class="hamburger-menu boards-menu-link"><i class="fa fa-bars"></i></a>
       </div>
     </div>
   </div>
