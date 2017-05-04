433ac057a36193a389b4dd6cfc8717453bb683be
Igor Amidzic <amidzicigor@yahoo.com>
2017-05-04 16:54:59 -0400

    Add menu and notifs dropdowns

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -5,25 +5,50 @@
     <!-- Boards and Search Form -->
     <div class="left-container col-lg-5 col-md-5 col-sm-5 col-xs-5">
       <div class="content-holder">
-        <a href="#" class="boards boards-menu-link"><i class="fa fa-th-large"></i> Boards</a>
+        <a href="#" class="nav-link boards boards-menu-link"><i class="fa fa-th-large"></i> Boards</a>
       </div>
     </div>
 
     <!-- Logo -->
     <div class="holder center-container col-lg-2 col-md-2 col-sm-2 col-xs-2">
       <div class="content-holder">
-        <a href="/" class="logo"><img src="img/trello-logo.png" alt=""></a>
+        <a href="/" class="nav-link logo"><img src="img/trello-logo.png" alt=""></a>
       </div>
     </div>
 
     <!-- Menu, Notifications, and Profile -->
     <div class="holder right-container col-lg-5 col-md-5 col-sm-5 col-xs-5">
       <div class="content-holder">
-        <div class="">
+        <!-- List of Links -->
+        <ul class="nav navbar-nav navbar-right">
+          <!-- Menu Dropdown -->
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle nav-link notifs-btn boards-menu-link" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+              <i class="fa fa-bell-o"></i>
+            </a>
+            <ul class="dropdown-menu">
+              <li><a href="#">Action</a></li>
+              <li><a href="#">Another action</a></li>
+              <li><a href="#">Something else here</a></li>
+              <li role="separator" class="divider"></li>
+              <li><a href="#">Separated link</a></li>
+            </ul>
+          </li>
 
-        </div>
-        <a href="#" class="notifs-btn boards-menu-link"><i class="fa fa-bell-o"></i></a>
-        <a href="#" class="hamburger-menu boards-menu-link"><i class="fa fa-bars"></i></a>
+          <!-- Notifications Dropdown -->
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle nav-link hamburger-menu boards-menu-link" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+              <i class="fa fa-bars"></i>
+            </a>
+            <ul class="dropdown-menu">
+              <li><a href="#">Action</a></li>
+              <li><a href="#">Another action</a></li>
+              <li><a href="#">Something else here</a></li>
+              <li role="separator" class="divider"></li>
+              <li><a href="#">Separated link</a></li>
+            </ul>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
