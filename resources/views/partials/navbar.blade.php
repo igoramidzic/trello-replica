f016dbdffe64fd854bae7bbe632c637b27507e1d
Igor Amidzic <amidzicigor@yahoo.com>
2017-05-04 13:31:35 -0400

    Start navbar

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,27 @@
+<!-- Nav -->
+<nav class="navbar navbar-default">
+  <!-- Content container -->
+  <div class="row container-fluid">
+    <!-- Boards and Search Form -->
+    <div class="holder left-container col-lg-5 col-md-5 col-sm-5 col-xs-5">
+      <div class="content-holder">
+        <a href="/" class="logo"><i class="fa fa-th-large"></i> Boards</a>
+      </div>
+    </div>
+
+    <!-- Logo -->
+    <div class="holder center-container col-lg-2 col-md-2 col-sm-2 col-xs-2">
+
+      <div class="content-holder">
+        <a href="/" class="logo ">Trello <i class="fa fa-angle-up"></i></a>
+      </div>
+    </div>
+
+    <!-- Menu, Notifications, and Profile -->
+    <div class="holder right-container col-lg-5 col-md-5 col-sm-5 col-xs-5">
+      <div class="content-holder">
+        <a href="/" class="logo"><i class="fa fa-th-large"></i> Boards</a>
+      </div>
+    </div>
+  </div>
+</nav>
