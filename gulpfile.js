a94a79833fcf4cca33200445d4c9a89e500a58c9
Igor Amidzic <amidzicigor@yahoo.com>
2017-05-05 00:00:33 -0400

    Add colored background thumbnails

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,6 @@ elixir(function (mix) {
     'main-content/main.sass',
     'main-content/main-navbar.sass',
     'main-content/main-items-content.sass',
-    'main-content/todo-navbar.sass',
     'main-content/user-item-content.sass'
   ], 'public/css/all.css') // Output path
 })
--- resources/views/partials/users-content.blade.php
+++ resources/views/partials/users-content.blade.php
@@ -5,29 +5,67 @@
 
     // Hard code users to test template
     $users = [
-      ["first_name" => "Igor", "last_name" => "Amidzic", "username" => "igoramidzic", "profile_img_url" => "img/profile-img.png"],
-      ["first_name" => "John", "last_name" => "Heimer", "username" => "johnheimer", "profile_img_url" => false],
-      ["first_name" => "Kahlie", "last_name" => "Solomid", "username" => "kahliesolomid", "profile_img_url" => false],
-      ["first_name" => "Antonio", "last_name" => "Steele", "username" => "antoniosteele", "profile_img_url" => false],
-      ["first_name" => "Frank", "last_name" => "Enstein", "username" => "frankenstein", "profile_img_url" => false],
-      ["first_name" => "Jasmine", "last_name" => "Holler", "username" => "jasmineholler", "profile_img_url" => false],
-      ["first_name" => "Archi", "last_name" => "Zephrahill", "username" => "archizephrahill", "profile_img_url" => false]
+      ["first_name" => "Igor", "last_name" => "Amidzic", "username" => "igoramidzic", "profile_img_url" => "img/profile-img.png", "color" => false],
+      ["first_name" => "John", "last_name" => "Heimer", "username" => "johnheimer", "profile_img_url" => false, "color" => "30, 156, 19"],
+      ["first_name" => "Kahlie", "last_name" => "Solomid", "username" => "kahliesolomid", "profile_img_url" => false, "color" => "164, 69, 22"],
+      ["first_name" => "Antonio", "last_name" => "Steele", "username" => "antoniosteele", "profile_img_url" => false, "color" => "72, 9, 144"],
+      ["first_name" => "Frank", "last_name" => "Enstein", "username" => "frankenstein", "profile_img_url" => false, "color" => "153, 0, 0"],
+      ["first_name" => "Jasmine", "last_name" => "Holler", "username" => "jasmineholler", "profile_img_url" => false, "color" => "42, 14, 14"],
+      ["first_name" => "Archi", "last_name" => "Zephrahill", "username" => "archizephrahill", "profile_img_url" => false, "color" => "70, 135, 255"],
+      ["first_name" => "John", "last_name" => "Heimer", "username" => "johnheimer", "profile_img_url" => false, "color" => "30, 156, 19"],
+      ["first_name" => "Kahlie", "last_name" => "Solomid", "username" => "kahliesolomid", "profile_img_url" => false, "color" => "164, 69, 22"],
+      ["first_name" => "Antonio", "last_name" => "Steele", "username" => "antoniosteele", "profile_img_url" => false, "color" => "72, 9, 144"],
+      ["first_name" => "Frank", "last_name" => "Enstein", "username" => "frankenstein", "profile_img_url" => false, "color" => "153, 0, 0"],
+      ["first_name" => "Jasmine", "last_name" => "Holler", "username" => "jasmineholler", "profile_img_url" => false, "color" => "42, 14, 14"],
+      ["first_name" => "Archi", "last_name" => "Zephrahill", "username" => "archizephrahill", "profile_img_url" => false, "color" => "70, 135, 255"],
+      ["first_name" => "John", "last_name" => "Heimer", "username" => "johnheimer", "profile_img_url" => false, "color" => "30, 156, 19"],
+      ["first_name" => "Kahlie", "last_name" => "Solomid", "username" => "kahliesolomid", "profile_img_url" => false, "color" => "164, 69, 22"],
+      ["first_name" => "Antonio", "last_name" => "Steele", "username" => "antoniosteele", "profile_img_url" => false, "color" => "72, 9, 144"],
+      ["first_name" => "Frank", "last_name" => "Enstein", "username" => "frankenstein", "profile_img_url" => false, "color" => "153, 0, 0"],
+      ["first_name" => "Jasmine", "last_name" => "Holler", "username" => "jasmineholler", "profile_img_url" => false, "color" => "42, 14, 14"],
+      ["first_name" => "Archi", "last_name" => "Zephrahill", "username" => "archizephrahill", "profile_img_url" => false, "color" => "70, 135, 255"],
+      ["first_name" => "John", "last_name" => "Heimer", "username" => "johnheimer", "profile_img_url" => false, "color" => "30, 156, 19"],
+      ["first_name" => "Kahlie", "last_name" => "Solomid", "username" => "kahliesolomid", "profile_img_url" => false, "color" => "164, 69, 22"],
+      ["first_name" => "Antonio", "last_name" => "Steele", "username" => "antoniosteele", "profile_img_url" => false, "color" => "72, 9, 144"],
+      ["first_name" => "Frank", "last_name" => "Enstein", "username" => "frankenstein", "profile_img_url" => false, "color" => "153, 0, 0"],
+      ["first_name" => "Jasmine", "last_name" => "Holler", "username" => "jasmineholler", "profile_img_url" => false, "color" => "42, 14, 14"],
+      ["first_name" => "Archi", "last_name" => "Zephrahill", "username" => "archizephrahill", "profile_img_url" => false, "color" => "70, 135, 255"],
+      ["first_name" => "John", "last_name" => "Heimer", "username" => "johnheimer", "profile_img_url" => false, "color" => "30, 156, 19"],
+      ["first_name" => "Kahlie", "last_name" => "Solomid", "username" => "kahliesolomid", "profile_img_url" => false, "color" => "164, 69, 22"],
+      ["first_name" => "Antonio", "last_name" => "Steele", "username" => "antoniosteele", "profile_img_url" => false, "color" => "72, 9, 144"],
+      ["first_name" => "Frank", "last_name" => "Enstein", "username" => "frankenstein", "profile_img_url" => false, "color" => "153, 0, 0"],
+      ["first_name" => "Jasmine", "last_name" => "Holler", "username" => "jasmineholler", "profile_img_url" => false, "color" => "42, 14, 14"],
+      ["first_name" => "Archi", "last_name" => "Zephrahill", "username" => "archizephrahill", "profile_img_url" => false, "color" => "70, 135, 255"],
+      ["first_name" => "John", "last_name" => "Heimer", "username" => "johnheimer", "profile_img_url" => false, "color" => "30, 156, 19"],
+      ["first_name" => "Kahlie", "last_name" => "Solomid", "username" => "kahliesolomid", "profile_img_url" => false, "color" => "164, 69, 22"],
+      ["first_name" => "Antonio", "last_name" => "Steele", "username" => "antoniosteele", "profile_img_url" => false, "color" => "72, 9, 144"],
+      ["first_name" => "Frank", "last_name" => "Enstein", "username" => "frankenstein", "profile_img_url" => false, "color" => "153, 0, 0"],
+      ["first_name" => "Jasmine", "last_name" => "Holler", "username" => "jasmineholler", "profile_img_url" => false, "color" => "42, 14, 14"],
+      ["first_name" => "Archi", "last_name" => "Zephrahill", "username" => "archizephrahill", "profile_img_url" => false, "color" => "70, 135, 255"],
+      ["first_name" => "John", "last_name" => "Heimer", "username" => "johnheimer", "profile_img_url" => false, "color" => "30, 156, 19"],
+      ["first_name" => "Kahlie", "last_name" => "Solomid", "username" => "kahliesolomid", "profile_img_url" => false, "color" => "164, 69, 22"],
+      ["first_name" => "Antonio", "last_name" => "Steele", "username" => "antoniosteele", "profile_img_url" => false, "color" => "72, 9, 144"],
+      ["first_name" => "Frank", "last_name" => "Enstein", "username" => "frankenstein", "profile_img_url" => false, "color" => "153, 0, 0"],
+      ["first_name" => "Jasmine", "last_name" => "Holler", "username" => "jasmineholler", "profile_img_url" => false, "color" => "42, 14, 14"],
+      ["first_name" => "Archi", "last_name" => "Zephrahill", "username" => "archizephrahill", "profile_img_url" => false, "color" => "70, 135, 255"],
 
     ];
 
     // Loop through each person
     for ($i = 0; $i < count($users); $i++) {
       // User Info Variables
-      $full_name = $users[$i]["first_name"] . " " . $users[$i]["last_name"];
-      $username = $users[$i]["username"];
-      $profile_img = $users[$i]["profile_img_url"];
+      $full_name      = $users[$i]["first_name"] . " " . $users[$i]["last_name"];
+      $username       = $users[$i]["username"];
+      $profile_img    = $users[$i]["profile_img_url"];
+      $color          = $users[$i]["color"];
     ?>
 
       <div class="item-holder col-lg-3 col-md-4 col-sm-6">
         <div class="user-item">
           <!-- User Thumbnail -->
-          <div class="user-thumbnail">
-            <?php
+          <?php
+          echo "<div class='user-thumbnail' style='color: rgb($color); background-color: rgba($color, 0.3)' >";
+
               if ($profile_img) {
                 echo "<img class='img-responsive img-circle' src=$profile_img />";
               }
