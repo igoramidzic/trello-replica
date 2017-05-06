414c072421f9a503f9f3ce26071b9c0dbad6183b
Igor Amidzic <amidzicigor@yahoo.com>
2017-05-06 12:49:39 -0400

    Add cards/profile links

--- resources/views/partials/users-content.blade.php
+++ resources/views/partials/users-content.blade.php
@@ -1,57 +1,47 @@
+<?php
+// Hard code users to test template
+$users = [
+  ["first_name" => "Igor", "last_name" => "Amidzic", "username" => "igoramidzic", "profile_img_url" => "img/profile-img.png", "color" => false, "taskCount" => 29],
+  ["first_name" => "John", "last_name" => "Heimer", "username" => "johnheimer", "profile_img_url" => false, "color" => "30, 156, 19", "taskCount" => 73],
+  ["first_name" => "Kahlie", "last_name" => "Solomid", "username" => "kahliesolomid", "profile_img_url" => false, "color" => "164, 69, 22", "taskCount" => 0],
+  ["first_name" => "Antonio", "last_name" => "Steele", "username" => "antoniosteele", "profile_img_url" => false, "color" => "72, 9, 144", "taskCount" => 14],
+  ["first_name" => "Frank", "last_name" => "Enstein", "username" => "frankenstein", "profile_img_url" => false, "color" => "153, 0, 0", "taskCount" => 8],
+  ["first_name" => "Jasmine", "last_name" => "Holler", "username" => "jasmineholler", "profile_img_url" => false, "color" => "42, 14, 14", "taskCount" => 63],
+  ["first_name" => "Archi", "last_name" => "Zephrahill", "username" => "archizephrahill", "profile_img_url" => false, "color" => "70, 135, 255", "taskCount" => 2]
+];
+?>
+
 <div class="items-content">
-
   <div class="row">
-    <?php
-
-    // Hard code users to test template
-    $users = [
-      ["first_name" => "Igor", "last_name" => "Amidzic", "username" => "igoramidzic", "profile_img_url" => "img/profile-img.png", "color" => false, "taskCount" => 29],
-      ["first_name" => "John", "last_name" => "Heimer", "username" => "johnheimer", "profile_img_url" => false, "color" => "30, 156, 19", "taskCount" => 73],
-      ["first_name" => "Kahlie", "last_name" => "Solomid", "username" => "kahliesolomid", "profile_img_url" => false, "color" => "164, 69, 22", "taskCount" => 0],
-      ["first_name" => "Antonio", "last_name" => "Steele", "username" => "antoniosteele", "profile_img_url" => false, "color" => "72, 9, 144", "taskCount" => 14],
-      ["first_name" => "Frank", "last_name" => "Enstein", "username" => "frankenstein", "profile_img_url" => false, "color" => "153, 0, 0", "taskCount" => 8],
-      ["first_name" => "Jasmine", "last_name" => "Holler", "username" => "jasmineholler", "profile_img_url" => false, "color" => "42, 14, 14", "taskCount" => 63],
-      ["first_name" => "Archi", "last_name" => "Zephrahill", "username" => "archizephrahill", "profile_img_url" => false, "color" => "70, 135, 255", "taskCount" => 2]
-
-    ];
-
-    // Loop through each person
-    for ($i = 0; $i < count($users); $i++) {
-      // User Info Variables
-      $full_name      = $users[$i]["first_name"] . " " . $users[$i]["last_name"];
-      $username       = $users[$i]["username"];
-      $profile_img    = $users[$i]["profile_img_url"];
-      $color          = $users[$i]["color"];
-      $taskCount      = $users[$i]["taskCount"];
-    ?>
-
+    <!-- Loop through all users -->
+    @foreach ($users as $user)
       <div class="item-holder col-lg-3 col-md-4 col-sm-6">
         <div class="user-item">
           <!-- User Thumbnail -->
-          <?php
-          echo "<div class='user-thumbnail' style='color: rgb($color); background-color: rgba($color, 0.3)' >";
-
-              if ($profile_img) {
-                echo "<img class='img-responsive img-circle' src=$profile_img />";
-              }
-              else {
-                echo $users[$i]["first_name"][0] . $users[$i]["last_name"][0];
-              }
-            ?>
+          <div class='user-thumbnail' style='color: rgb({{ $user["color"] }}); background-color: rgba({{ $user["color"] }}, 0.3)' >
+            @if ($user["profile_img_url"])
+                <img class='img-responsive img-circle' src={{ $user["profile_img_url"] }} />
+            @else
+                {{ $user["first_name"][0] . $user["last_name"][0] }}
+            @endif
           </div>
           <!-- User's Name Info -->
           <div class="user-name">
-            <h4 class="name"><?php echo $full_name; ?></h5>
-            <h6 class="username">@<?php echo $username; ?></h6>
+            <h4 class="name">{{ $user["first_name"] }}</h5>
+            <h6 class="username">{{ "@" . $user["username"] }}</h6>
           </div>
           <!-- Number of Tasks -->
           <div class="tasksHolder">
-            <h1 class="taskCount"><?php echo $taskCount; ?></h1>
+            <h1 class="taskCount">{{ $user["taskCount"] }}</h1>
             <p class="description">Open Tasks</p>
+          </div>
+          <!-- User Links -->
+          <div class="container-fluid linksHolder">
+            <a class="col-xs-6" href="#"><i class="icon fa fa-check-square-o"></i> CARDS</a>
+            <a class="col-xs-6" href="#"><i class="icon fa fa-user-o"></i> PROFILE</a>
           </div>
         </div>
       </div>
-
-    <?php } ?>
+    @endforeach
   </div>
 </div>
