49cc62421d140a23b1799c1743413c43fd16b832
Igor Amidzic <amidzicigor@yahoo.com>
2017-05-12 23:33:55 -0400

    Fix profile page for non-loggedin users

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -8,11 +8,13 @@
 
       @include ('partials.main-navbar')
 
-      @if (Auth::user() AND Auth::user()->username == $user->username)
-        Welcome to your profile, {{ Auth::user()->first_name }}
-      @elseif ($user)
-        <!-- If statement checks if user's first name ends in 's' -->
-        This is {{ $user->first_name }}'@if ($user->first_name[strlen($user->first_name) - 1] != 's')s @endif profile
+      @if (isset($user))
+        @if (Auth::user() AND Auth::user()->username == $user->username)
+          Welcome to your profile, {{ Auth::user()->first_name }}
+        @else
+          <!-- If statement checks if user's first name ends in 's' -->
+          This is {{ $user->first_name }}'@if ($user->first_name[strlen($user->first_name) - 1] != 's')s @endif profile
+        @endif
       @else
         @include ('partials.page-not-found')
       @endif
